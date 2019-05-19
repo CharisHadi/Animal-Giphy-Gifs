36352f375279ffcd9d3679ba2741f25caec84289
CharisHadi <charishadi@gmail.com>
2019-05-18 21:51:19 -0700

    gif make, play and pause

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var animals = [];
-
+var results = [];
 var key = "gwhUjMEXBLwI5uRQ7smH8e4TTJqEN3yd";
 
 // Function for displaying animal data
@@ -29,12 +29,12 @@ $(document.body).on("click", ".animal-button", function(){
     var animal = this.id;
     // console.log(this.id);
     var qeuryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + animal + "&limit=10&rating=PG-13&lang=en";
-
+    //ajax call to get gif links and create elements for them to prepend to gif area
     $.ajax({
         url: qeuryURL,
         method: "GET"
     }).then(function(response){
-        var results = response.data;
+        results = response.data;
         console.log(results);
         for (var i = 0; i < results.length; i++) {
           var gifDiv = $("<div>");
@@ -44,7 +44,7 @@ $(document.body).on("click", ".animal-button", function(){
           var p = $("<p>").text("Rating: " + rating);
 
           var animalGif = $("<img>");
-          animalGif.attr("id", animal + "-gif");
+          animalGif.attr("id", i);
           animalGif.attr("class", "gif");
           animalGif.attr("src", results[i].images.fixed_height_still.url);
 
@@ -55,11 +55,22 @@ $(document.body).on("click", ".animal-button", function(){
         }
     });
 
-    
 });
 
+
 $(document.body).on("click", ".gif", function(){
-    
+    var url = this.src;
+    var index = this.id
+
+
+    if(url === results[index].images.fixed_height_still.url){
+        this.src = results[index].images.fixed_height.url;
+    }
+
+    else{
+        this.src = results[index].images.fixed_height_still.url;
+    }
+
 });
 
 // Calling the renderButtons function to display the initial list of animals
